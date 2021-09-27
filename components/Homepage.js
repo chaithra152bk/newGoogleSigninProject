0d2bbf15616aeaafba7304c156900baf47cc00ef
chaithraNayak <chaithrabk152@gmail.com>
2021-09-27 22:16:13 +0530

    changes added

--- components/Homepage.js
+++ components/Homepage.js
@@ -1,23 +1,67 @@
 import * as React from 'react';
 import {
-    SafeAreaView,
-    ScrollView,
-    StatusBar,
-    StyleSheet,
-    Text,
-    useColorScheme,
-    View,
-  } from 'react-native';
-  
-const Stack = createNativeStackNavigator();
+  Text,
+  View,
+  TouchableOpacity
+} from 'react-native';
+import {
+  GoogleSignin
+} from '@react-native-google-signin/google-signin';
 
-const HomePageScreen = () => {
+
+const HomePageScreen = (props) => {
+  const { containerStyle, welcomeTextStyle, logoutButtonContainer, logoutText } = styles;
+  const _signOut = async () => {
+    try {
+      await GoogleSignin.signOut();
+      props.navigation.navigate("Login")
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
-    <View>
-        <Text>LoginScrenn</Text>
+    <View style={containerStyle}>
+      <View>
+        <Text style={welcomeTextStyle}>Welcome to HomePage</Text>
+      </View>
+      <TouchableOpacity onPress={_signOut}>
+        <View style={logoutButtonContainer}>
+          <Text style={logoutText}>Logout</Text>
+        </View>
+      </TouchableOpacity>
+
     </View>
   );
 };
 
+
+const styles = {
+  containerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    alignItems: 'center'
+  },
+  welcomeTextStyle: {
+    textAlign: 'center',
+    fontSize: 32,
+    color: 'black',
+    fontWeight: 'bold'
+
+  },
+  logoutButtonContainer: {
+    justifyContent: 'flex-end',
+    marginTop: 150,
+    padding: 20,
+    backgroundColor: 'red'
+  },
+  logoutText:{
+    textAlign: 'center',
+    fontSize: 18,
+    color: 'white',
+    fontWeight: 'bold'
+  }
+};
 
 export default HomePageScreen;
--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,23 +1,76 @@
 import * as React from 'react';
 import {
-    SafeAreaView,
-    ScrollView,
-    StatusBar,
-    StyleSheet,
-    Text,
-    useColorScheme,
-    View,
-  } from 'react-native';
-  
-const Stack = createNativeStackNavigator();
+  View
+} from 'react-native';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
 
-const LoginScreen = () => {
+
+const LoginScreen = (props) => {
+  const { containerStyle, buttonContainer, buttonStyle } = styles;
+
+  GoogleSignin.configure({
+    webClientId: '236253973910-rvctbr2k01n7ildfsd28cefa0c1n86hh.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+    offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+  });
+
+
+  const _signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log("userInfo", userInfo)
+      props.navigation.navigate("Homepage")
+
+    } catch (error) {
+      console.log("erooor", error)
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
   return (
-    <View>
-        <Text>LoginScrenn</Text>
+    <View style={containerStyle}>
+
+      <View style={buttonContainer}>
+        <GoogleSigninButton
+          style={buttonStyle}
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Dark}
+          onPress={_signIn}
+        />
+      </View>
     </View>
   );
 };
 
+
+const styles = {
+  containerStyle: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    backgroundColor: 'white'
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  buttonStyle: {
+    width: 300,
+    height: 65,
+    marginTop: 100,
+
+  }
+};
 
 export default LoginScreen;
